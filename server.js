106f5df500e1caa603efb6424ef10c6c386bc90b
KUWRITERSHUB <globegainpipsprowriters@gmail.com>
2026-03-25 05:57:28 +0300

    Update server.js
    const express = require("express");
    const app = express();
    
    app.use(express.json());
    
    app.get("/", (req, res) => {
      res.send("UK Writers Hub Backend Running ✅");
    });
    
    app.post("/pay", (req, res) => {
      const phone = req.body.phone;
    
      console.log("Payment request from:", phone);
    
      res.json({
        message: "STK Push will be triggered here",
        phone: phone
      });
    });
    
    app.listen(3000, () => console.log("Server running"));

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("UK Writers Hub Backend is running ✅");
+  res.send("UK Writers Hub Backend Running ✅");
 });
 
 app.post("/pay", (req, res) => {
   const phone = req.body.phone;
-  console.log("Phone:", phone);
 
-  res.json({ message: "STK Push will be sent here" });
+  console.log("Payment request from:", phone);
+
+  res.json({
+    message: "STK Push will be triggered here",
+    phone: phone
+  });
 });
 
 app.listen(3000, () => console.log("Server running"));
