2d9f1164f00006865602d5a34f5f294af5ba0087
KUWRITERSHUB <globegainpipsprowriters@gmail.com>
2026-04-27 15:19:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-console.log("SERVER STARTING...");
 
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("M-PESA Server Running ✅");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
